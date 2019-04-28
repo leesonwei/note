e633a19d4db1d2ce43d07acae6fda41667cbbba0
anywhere <weilizong2014@hotmail.com>
2019-04-28 21:37:32 +0800

    auth first commite

---
+++ auth/src/main/java/com/anywhere/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package com.anywhere.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
