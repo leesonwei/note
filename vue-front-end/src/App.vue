b571c5a25753da4696a2b829404e6a331ff5de67
LIZONG.WEI <weilizong2014@hotmailcom>
2019-05-07 17:28:26 +0800

    vue init

--- vue-front-end/src/App.vue
+++ vue-front-end/src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Login></Login>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'App'
-}
+  import Login from '@/components/Login'
+  export default {
+    name: 'App',
+    components: {
+      'Login': Login
+    },
+    data() {
+      return {}
+    }
+  }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    text-align: center;
+    margin-top:400px ;
+  }
 </style>
