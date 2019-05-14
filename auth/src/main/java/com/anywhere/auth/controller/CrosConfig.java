62c7fd9824b4b9a12fe208804adbddfea0521225
LIZONG.WEI <weilizong2014@hotmailcom>
2019-05-14 18:56:29 +0800

    tagsinput added

---
+++ auth/src/main/java/com/anywhere/auth/controller/CrosConfig.java
@@ -0,0 +1,31 @@
+package com.anywhere.auth.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author LIZONG.WEI
+ * @version V1.0
+ * @description
+ * @date 2019/5/8 9:44
+ */
+@Configuration
+public class CrosConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
+}
--- auth/src/main/java/com/anywhere/auth/controller/ResttController.java
+++ auth/src/main/java/com/anywhere/auth/controller/ResttController.java
@@ -1,6 +1,7 @@
 package com.anywhere.auth.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,15 @@ public class ResttController {
         map.put("rest", "rest");
         map.put("name",name);
         return map;
+    }
+
+    @RequestMapping("/authorication/login")
+    @ResponseBody
+    public String login(@RequestParam("userName") String userName, @RequestParam("password") String password){
+        System.out.println(userName);
+        if (userName != null && userName.equals(password)) {
+            return "login success";
+        }
+        return "login fail";
     }
 }
